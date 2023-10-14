dd2d1e7f11e5bce27da4cbb183e13a9334a959ae
Nutty-Nuts <vegagerard1130@gmail.com>
2023-10-14 12:20:18 +0800

    implemented card shuffling

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -19,5 +19,15 @@ function mapCards(characters, click) {
         );
     });
 
-    return <>{cards}</>;
+    return <>{shuffle(cards)}</>;
+}
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
 }
