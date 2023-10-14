a3385f2c087fae18f821ab381dc8d6d3e2f80980
Nutty-Nuts <vegagerard1130@gmail.com>
2023-10-14 10:59:05 +0800

    react project init

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,35 @@
-import React, { useState, useEffect } from "react";
-import Cards from "./components/Cards";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-    const [score, setScore] = useState(0);
-    const [bestScore, setBestScore] = useState(0);
-    const [message, setMessage] = useState("Game!");
-    const [pick, setPick] = useState({
-        first: false,
-        second: false,
-        third: false,
-        fourth: false,
-    });
+  const [count, setCount] = useState(0)
 
-    function incrementScore() {
-        setScore(score + 1);
-    }
-
-    function toggleCard(id) {
-        setPick((pick) => ({
-            ...pick,
-            [id]: true,
-        }));
-    }
-
-    function resetGame() {
-        setBestScore(score);
-        setScore(0);
-        setPick((pick) => ({
-            first: false,
-            second: false,
-            third: false,
-            fourth: false,
-        }));
-    }
-
-    function handleClick(e) {
-        let id = [e.target.id];
-        if (!pick[id]) {
-            incrementScore();
-            toggleCard(id);
-            setMessage("Game!");
-        } else {
-            setMessage("Try Again!");
-            resetGame();
-        }
-    }
-
-    useEffect(() => {
-        if (score == 4) {
-            setBestScore(4);
-            setMessage("You Win!");
-        }
-    });
-
-    return (
-        <div>
-            <p>Best Score: {bestScore}</p>
-            <p>Score: {score}</p>
-            <p>{message}</p>
-            <Cards click={handleClick} />
-        </div>
-    );
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
